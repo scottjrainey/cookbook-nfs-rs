21a1cd4c2358f87f0a514a85b9838c6c916270cd
scottjrainey <scottjrainey@gmail.com>
2013-05-23 16:27:08 -0600

    Added line and nfs cookbooks

---
+++ cookbooks/line/metadata.rb
@@ -0,0 +1,8 @@
+name             "line"
+maintainer       "Scott Rainey"
+maintainer_email "scott@scottjrainey.com"
+license          "Apache2"
+description      "Provides 'line-cookbook' line editing resources for use by recipes run on Rightscale"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
